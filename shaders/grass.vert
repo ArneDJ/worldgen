ddd9ed47d70d8eda67866bf7b4ec29ac2c23b48d
ArneDJ <ArneDeJonghe@protonmail.com>
2020-08-08 12:21:52 +0200

    clean

--- shaders/grass.vert
+++
@@ -1,8 +0,0 @@
-#version 430 core
-
-layout(location = 0) in vec2 coordinate;
-
-void main(void)
-{
-	gl_Position = vec4(coordinate.x, 0.0, coordinate.y, 1.0);
-}
--- src/worldmap/terraform.h
+++
@@ -1,9 +0,0 @@
-class Terraform {
-public:
-	struct floatimage heightmap;
-	struct byteimage temperature;
-	struct byteimage rainfall;
-public:
-	Terraform(size_t side, long seed, float freq);
-	~Terraform(void);
-};
